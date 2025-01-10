855bcbc562b7c428bc83e68e8cfd1138de408499
VforWedth <lorendrain47@gmail.com>
2025-01-10 15:04:18 +0630

    Home Page and Sign-in

--- inferno/userauths/forms.py
+++ inferno/userauths/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from userauths.models import User
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Username"}))
      
     class Meta:
         model = User
